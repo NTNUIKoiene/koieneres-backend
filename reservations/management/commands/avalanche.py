b557a51e24ef003f46f4d455ac03be67c538a3b4
Espen Meidell <espen.meidell@gmail.com>
2019-10-02 21:30:17 +0200

    Close cabin on affected dates

--- reservations/management/commands/avalanche.py
+++ reservations/management/commands/avalanche.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 from urllib import request
 import json
 from utils.dateutils import compute_reservation_period, string_to_date
-from reservations.models import ExtendedPeriod
+from reservations.models import ExtendedPeriod, Cabin, CabinClosing
 
 
 class Command(BaseCommand):
@@ -13,17 +13,43 @@ class Command(BaseCommand):
         from_date = str(reservation_period["from"])
         to_date = str(reservation_period["to"])
 
-        data = request.urlopen(
-            f"https://api01.nve.no/hydrology/forecast/avalanche/v4.0.2/api/AvalancheWarningByRegion/Simple/3022/2/{from_date}/{to_date}"
-        )
+        nve_url = f"https://api01.nve.no/hydrology/forecast/avalanche/v4.0.2/api/AvalancheWarningByRegion/Simple/3022/2/{from_date}/{to_date}"
+        mock_url = "http://www.mocky.io/v2/5d94f6972f00006d008ffa62"
+
+        # Load and process avalanche data
+        data = request.urlopen(mock_url)
         data = json.load(data)
-        data = map(
-            lambda warning: (
-                string_to_date(warning["ValidFrom"].split("T")[0]),
-                int(warning["DangerLevel"]),
-            ),
-            data,
+        data = list(
+            filter(
+                lambda warning: warning[1] >= 3,
+                map(
+                    lambda warning: (
+                        string_to_date(warning["ValidFrom"].split("T")[0]),
+                        int(warning["DangerLevel"]),
+                    ),
+                    data,
+                ),
+            )
         )
-        print(list(data))
+
+        if len(data) == 0:
+            self.stdout.write(self.style.SUCCESS("No warnings above 3 found"))
+            self.stdout.write(
+                self.style.SUCCESS("Successfully checked avalanche warnings")
+            )
+            return
+
+        kamtjonn = Cabin.objects.get(name="Kamtjønnkoia")
+        # Close cabin on dates
+        for date, level in data:
+            self.stdout.write(f"Closing on {str(date)}. Warning level {level}")
+            closing = CabinClosing(
+                cabin=kamtjonn,
+                from_date=date,
+                to_date=date,
+                comment=f"Avalanche Warning",
+            )
+            closing.save()
+
         self.stdout.write(self.style.SUCCESS("Successfully checked avalanche warnings"))
 
